3846916ad3eaa97338ce74fdcc95b1c43895f188
HeYichigo <he_yichigosann@outlook.com>
2026-03-16 11:05:41 +0800

    # 前端部署配置文件

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     AButton: typeof import('ant-design-vue/es')['Button']
     ACard: typeof import('ant-design-vue/es')['Card']
     ACardMeta: typeof import('ant-design-vue/es')['CardMeta']
-    ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
     ADrawer: typeof import('ant-design-vue/es')['Drawer']
     AFloatButton: typeof import('ant-design-vue/es')['FloatButton']
     AForm: typeof import('ant-design-vue/es')['Form']
